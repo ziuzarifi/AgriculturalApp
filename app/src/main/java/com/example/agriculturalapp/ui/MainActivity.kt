f7c03162b20523b69033808608c80a24493d0d21
ziuzarifi <zamir.zaripov.01@gmail.com>
2022-06-16 17:01:32 +0500

    setup UI mainActivity and categoriesFragment: bottomNav, toolbar, statusBar, searchView

--- app/src/main/java/com/example/agriculturalapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/agriculturalapp/ui/MainActivity.kt
@@ -2,11 +2,53 @@ package com.example.agriculturalapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.example.agriculturalapp.R
+import com.example.agriculturalapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    var navController: NavController? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navController = findNavController(R.id.fragmentContainerView)
+
+        binding.bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.categories -> navController?.navigate(R.id.categoriesFragment)
+
+                R.id.addingAds -> navController?.navigate(R.id.addingAdsFragment)
+
+                R.id.saved -> navController?.navigate(R.id.savedFragment)
+
+                R.id.account -> navController?.navigate(R.id.accountFragment)
+            }
+            true
+        }
     }
+
+    override fun onBackPressed() {
+        when(navController?.currentDestination?.id){
+            R.id.categoriesFragment, R.id.addingAdsFragment, R.id.savedFragment, R.id.accountFragment -> {
+                AlertDialog.Builder(this)
+                    .setTitle("Вы уверены,")
+                    .setMessage("что хотите выйти?")
+                    .setNegativeButton("Отмена", null)
+                    .setPositiveButton("Да"){ dialogInterface, i ->
+                        finish()
+                    }
+                    .create()
+                    .show()
+            }
+        }
+        super.onBackPressed()
+    }
+
 }
